f8688825a5c66ed6eb27e29a11bcddb3dcc3639b
ktanojo <ktanojo@umich.edu>
2020-09-06 00:18:02 -0400

    finished setting up react and JSX

---
+++ src/form.js
@@ -0,0 +1,11 @@
+class form extends Component {
+    state = { 
+     }
+    // Need a state for each of the form pages 
+    render() { // render does all the html css shit, import css file 
+        return ( );
+    }
+}
+ 
+// big form render, smaller forms export 
+export default form;
